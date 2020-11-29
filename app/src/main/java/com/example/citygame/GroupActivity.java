36b2f248ef06ed4ae6a26943e647d4f42abf8403
annadamczyk <annam42m17@gmail.com>
2020-11-30 00:00:56 +0100

    a few changes in the view - creating a new path

--- app/src/main/java/com/example/citygame/GroupActivity.java
+++ app/src/main/java/com/example/citygame/GroupActivity.java
@@ -3,17 +3,24 @@ package com.example.citygame;
 import android.Manifest;
 import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationListener;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.ArrayAdapter;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -77,7 +84,7 @@ public class GroupActivity extends AppCompatActivity implements LocationListener
         MapEventsReceiver mReceive = new MapEventsReceiver() {
             @Override
             public boolean singleTapConfirmedHelper(GeoPoint p) {
-                Toast.makeText(GroupActivity.this, "one click", Toast.LENGTH_SHORT).show();
+                Toast.makeText(GroupActivity.this, "Musisz dłużej przytrzymać by dodać punkt na mapie.", Toast.LENGTH_SHORT).show();
                 return false;
             }
 
@@ -125,7 +132,7 @@ public class GroupActivity extends AppCompatActivity implements LocationListener
                 if(index == -1) {
                     addQuestionsToMarker(markerInfo);
                 } else {
-                    editMarker(path.get(index));
+                    editMarker(path.get(index), index);
                 }
                 return true;
             }
@@ -133,28 +140,61 @@ public class GroupActivity extends AppCompatActivity implements LocationListener
         mapView.getOverlays().add(marker);
     }
 
-    public void editMarker(final Marker marker){
+    public void editMarker(final Marker marker, final int index){
         AlertDialog.Builder pictureDialog = new AlertDialog.Builder(this);
         LayoutInflater layoutInflater = this.getLayoutInflater();
         final View view = layoutInflater.inflate(R.layout.popup_edit_marker, null);
 
-        ArrayAdapter<String> adapter;
-        ListView listOfAnswers = (ListView) view.findViewById(R.id.answersList);
-        adapter = new ArrayAdapter<String>(this,
-                R.layout.item_list,
-                R.id.answerContent,
-                marker.question.answers);
+        final ListView listOfAnswers = (ListView) view.findViewById(R.id.answersList);
+
+        final MyCustomAdapter adapter = new MyCustomAdapter(marker.question.answers, this, path.indexOf(marker), 2);
         listOfAnswers.setAdapter(adapter);
+
+        listOfAnswers.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                String item = marker.question.answers.get(position);
+                marker.question.correctAnswer = item;
+
+                for (int a = 0; a < listOfAnswers.getChildCount(); a++) {
+                    if(position == a ){
+                        listOfAnswers.getChildAt(a).setBackgroundColor(Color.parseColor("#16951F"));
+                        Toast.makeText(GroupActivity.this, "Zaznaczyłeś prawidłową odpowiedź.", Toast.LENGTH_SHORT).show();
+                    }else{
+                        listOfAnswers.getChildAt(a).setBackgroundColor(Color.TRANSPARENT);
+                    }
+                }
+            }
+        });
 
         final EditText contentQuestion = view.findViewById(R.id.editTextContentEdit);
         final EditText titleMarker = view.findViewById(R.id.editTextMarkerNameEdit);
+        final EditText newAnswer = view.findViewById(R.id.editTextNewAnswer);
+
+        ImageButton addNewAnswer = view.findViewById(R.id.imageButtonAddAnswer);
+        addNewAnswer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                if(newAnswer.getText().toString() != "" && newAnswer.getText().toString() != null) {
+                    adapter.list.add(newAnswer.getText().toString());
+                    marker.question.answers.add(newAnswer.getText().toString());
+                    adapter.notifyDataSetChanged();
+                }
+            }
+        });
 
         Button btnEditMarker = view.findViewById(R.id.btnEditMarker);
         btnEditMarker.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view)
             {
+                Marker m = new Marker(marker.id, marker.lat, marker.lon, marker.title);
+                m.question = new QuestionModel(marker.question.content, marker.question.correctAnswer, new ArrayList<>(marker.question.answers));
+
+                marker.title = titleMarker.getText().toString();
                 marker.question = new QuestionModel(contentQuestion.getText().toString(),"", marker.question.answers);
+                submitChanges(m, index);
             }
         });
 
@@ -163,6 +203,28 @@ public class GroupActivity extends AppCompatActivity implements LocationListener
 
         pictureDialog.setView(view);
         pictureDialog.show();
+    }
+
+    private void submitChanges(final Marker marker, final int index) {
+        AlertDialog.Builder dialog=new AlertDialog.Builder(this);
+        dialog.setMessage("Czy chcesz zatwierdzić wprowadzone dane?");
+        dialog.setTitle("Potwierdzenie");
+        dialog.setPositiveButton("TAK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,
+                                        int which) {
+
+                    }
+                });
+        dialog.setNegativeButton("NIE",new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                path.get(index).title = marker.title;
+                path.get(index).question = new QuestionModel(marker.question.content, marker.question.correctAnswer, marker.question.answers);
+            }
+        });
+        AlertDialog alertDialog=dialog.create();
+        alertDialog.show();
     }
 
     public int isEmpty(Marker marker){
@@ -176,8 +238,9 @@ public class GroupActivity extends AppCompatActivity implements LocationListener
     }
 
     public void addQuestionsToMarker(final Marker marker){
+        final String[] correctAnswer = new String[1];
         final List<String> answers = new ArrayList<>();
-        AlertDialog.Builder pictureDialog = new AlertDialog.Builder(this);
+        final AlertDialog.Builder pictureDialog = new AlertDialog.Builder(this);
 
         LayoutInflater layoutInflater = this.getLayoutInflater();
         final View view = layoutInflater.inflate(R.layout.popup_new_question, null);
@@ -186,8 +249,33 @@ public class GroupActivity extends AppCompatActivity implements LocationListener
         final EditText contentQuestion = view.findViewById(R.id.editTextContent);
         final EditText titleMarker = view.findViewById(R.id.editTextMarkerName);
 
-        Button btnaddAnswer = view.findViewById(R.id.btnAddAnswer);
+        ImageButton btnaddAnswer = view.findViewById(R.id.btnAddAnswer);
         Button btnaddQuestion = view.findViewById(R.id.btnAddQuestion);
+
+        final ListView listOfAnswers = view.findViewById(R.id.answersListNew);
+
+        final MyCustomAdapter adapter = new MyCustomAdapter(answers, this, path.indexOf(marker), 1);
+        listOfAnswers.setAdapter(adapter);
+
+        listOfAnswers.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                 correctAnswer[0] = answers.get(position);
+
+                for (int a = 0; a < listOfAnswers.getChildCount(); a++) {
+                    if(position == a ){
+                        listOfAnswers.getChildAt(a).setBackgroundColor(Color.parseColor("#16951F"));
+                        Toast.makeText(GroupActivity.this, "Zaznaczyłeś prawidłową odpowiedź.", Toast.LENGTH_SHORT).show();
+                    }else{
+                        listOfAnswers.getChildAt(a).setBackgroundColor(Color.TRANSPARENT);
+                    }
+                }
+            }
+        });
+
+        pictureDialog.setView(view);
+        final AlertDialog dialog = pictureDialog.create();
+        dialog.show();
 
         btnaddAnswer.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -196,6 +284,8 @@ public class GroupActivity extends AppCompatActivity implements LocationListener
                 String answer = answerText.getText().toString();
                 if(answer != "" && answer != null){
                     answers.add(answer);
+                    adapter.list.add(answer);
+                    adapter.notifyDataSetChanged();
                 }
 
                 answerText.setText("");
@@ -206,16 +296,14 @@ public class GroupActivity extends AppCompatActivity implements LocationListener
             @Override
             public void onClick(View view)
             {
-                marker.question = new QuestionModel(contentQuestion.getText().toString(),"", answers);
+                marker.question = new QuestionModel(contentQuestion.getText().toString(), correctAnswer[0], answers);
                 marker.title = titleMarker.getText().toString();
 
                 path.add(marker);
+                dialog.dismiss();
                 //TODO: save QUESTION to server
             }
         });
-
-        pictureDialog.setView(view);
-        pictureDialog.show();
     }
 
     @Override
@@ -241,5 +329,67 @@ public class GroupActivity extends AppCompatActivity implements LocationListener
     @Override
     public void onMapLongClick(LatLng latLng) {
         Toast.makeText(this, "help", Toast.LENGTH_SHORT).show();
+    }
+
+    class MyCustomAdapter extends BaseAdapter implements ListAdapter {
+        public ArrayList<String> list;
+        private Context context;
+        private int index;
+        private int mode;
+
+        public MyCustomAdapter(List<String> list, Context context, int index, int mode) {
+            this.list = new ArrayList<>(list);
+            this.context = context;
+            this.index = index;
+            this.mode = mode;
+        }
+
+        @Override
+        public int getCount() {
+            return list.size();
+        }
+
+        @Override
+        public Object getItem(int pos) {
+            return list.get(pos);
+        }
+
+        @Override
+        public long getItemId(int pos) {
+            return list.indexOf(pos);
+        }
+
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+            View view = convertView;
+            if (view == null) {
+                LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                view = inflater.inflate(R.layout.item_list, null);
+            }
+
+            final ListView listOfAnswers = (ListView) view.findViewById(R.id.answersList);
+
+            TextView listItemText = view.findViewById(R.id.answerContent);
+            listItemText.setText(list.get(position));
+
+            ImageButton btnDelete = view.findViewById(R.id.imageButtonDelete);
+            if (mode == 1){
+                btnDelete.setVisibility(View.INVISIBLE);
+            } else if (mode == 2){
+
+            }
+
+            btnDelete.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    list.remove(position); //or some other task
+                    //TODO zmienić sposób usuwania odp z punktu ścieżki
+                    path.get(index).question.answers.remove(position);
+                    notifyDataSetChanged();
+                }
+            });
+
+            return view;
+        }
     }
 }
